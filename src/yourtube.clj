194fb3e0a315977af00b2991eb36ce3c2292da68
Guilherme Ribeiro <guilhermelionribeiro@gmail.com>
2023-05-21 17:00:57 -0300

    adds simple build script

--- src/yourtube.clj
+++ src/yourtube.clj
@@ -1,7 +1,8 @@
 (ns yourtube
   (:require [clj-http.client :as client])
   (:require [clojure.data.json :as json])
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io])
+  (:gen-class))
 
 (def get-info-url "https://%79%6F%75%74%75%62%65%69%2E%67%6F%6F%67%6C%65%61%70%69%73%2E%63%6F%6D/%79%6F%75%74%75%62%65%69/v1/%70%6C%61%79%65%72?%6B%65%79=%41%49%7A%61%53%79%41%4F%5F%46%4A%32%53%6C%71%55%38%51%34%53%54%45%48%4C%47%43%69%6C%77%5F%59%39%5F%31%31%71%63%57%38")
 
